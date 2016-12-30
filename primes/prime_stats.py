880ba0d00260866d292e6e5e2ba2b4f3c0588303
mikemey <miguel.maestro@gmx.co.uk>
2016-12-30 20:55:04 +0100

    removed unrelated script.

--- primes/prime_stats.py
+++
@@ -1,8 +0,0 @@
-class PrimeCalculator(object):
-    def start(self):
-        """Start calculating prime numbers"""
-        raise NotImplementedError("not implemented.")
-
-    def next(self):
-        """Should be called to indicate a new prime number."""
-        raise NotImplementedError("not implemented.")
