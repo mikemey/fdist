c0919f45cae8d1a2486a9cc744c3c7e9a3c9811b
mikemey <miguel.maestro@gmx.co.uk>
2017-01-01 10:30:04 +0100

    moved global properties into separate file.

--- fdist/__init__.py
+++ fdist/__init__.py
@@ -1,4 +1,3 @@
-import logging
 import sys
 import time
 
@@ -8,22 +7,16 @@ from announcer import Announcer
 from file_loader import FileLoaderProvider
 from file_master import FileMaster
 from files_diff import FilesDiff
+from globals import *
 from local_files import LocalFiles
 from remote_files import RemoteFiles
 
 
-def init_logging(level=logging.INFO):
+def init_logging(level=LOG_LEVEL):
     logging.basicConfig(level=level,
                         format='%(asctime)s %(levelname)5s [%(name)-12s] %(message)s',
                         stream=sys.stdout
                         )
-
-
-FE_PORT = 18000
-BC_PORT = 18010
-DEFAULT_INTERVAL = 3
-
-LOCAL_DIR = "tmp"
 
 
 def main():
@@ -32,9 +25,9 @@ def main():
     master = FileMaster.start(FileLoaderProvider())
     file_diff = FilesDiff.start(master)
 
-    broadcaster = Announcer.start(FE_PORT, BC_PORT, DEFAULT_INTERVAL)
-    LocalFiles.start([broadcaster, file_diff], LOCAL_DIR, DEFAULT_INTERVAL)
-    remote = RemoteFiles(file_diff, BC_PORT).start()
+    broadcaster = Announcer.start(FILE_EXCHANGE_PORT, BROADCAST_PORT, BROADCAST_INTERVAL_SEC)
+    LocalFiles.start([broadcaster, file_diff], SHARE_DIR, LOCAL_FILES_INTERVAL_SEC)
+    remote = RemoteFiles(file_diff, BROADCAST_PORT).start()
 
     logging.info('FDIST started')
     try:
