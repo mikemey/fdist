c78874d27b145ad590c40d16965971b4d02f9439
mikemey <miguel.maestro@gmx.co.uk>
2017-01-23 17:16:59 +0000

    init script starts file-exchange-server now.

--- fdist/__init__.py
+++ fdist/__init__.py
@@ -4,7 +4,7 @@ import time
 from pykka.registry import ActorRegistry
 
 from announcer import Announcer
-from exchange.file_info_server import FileInfoServer
+from exchange.file_exchange_server import FileExchangeServer
 from exchange.file_loader import create_file_loader
 from file_master import FileMaster
 from files_diff import FilesDiff
@@ -31,7 +31,7 @@ def main():
 
     local_file_receiver = [file_diff]
     if not RECEIVER_ONLY:
-        FileInfoServer.start(FILE_EXCHANGE_PORT, PIP_SIZE)
+        FileExchangeServer.start(FILE_EXCHANGE_PORT, SHARE_DIR, PIP_SIZE)
         file_announcer = Announcer.start(FILE_EXCHANGE_PORT, BROADCAST_PORT, BROADCAST_INTERVAL_SEC)
         local_file_receiver.append(file_announcer)
 
