f36ebccb2ee622eeda354f30d2aa8610b3b376fc
mikemey <miguel.maestro@gmx.co.uk>
2017-01-25 19:30:35 +0000

    continue reading when EAGAIN.

--- fdist/exchange/__init__.py
+++ fdist/exchange/__init__.py
@@ -27,17 +27,13 @@ if "darwin" == sys.platform:
 
 def read_data_from(connection):
     buf = ''
-    loops = 3
-    while loops > 0:
-        loops -= 1
+    while True:
         try:
-            while True:
-                chunk = connection.recv(CHUNK_SIZE)
-                bytes_read = len(chunk)
-                buf += chunk
-                if bytes_read < CHUNK_SIZE:
-                    break
-            return buf
+            chunk = connection.recv(CHUNK_SIZE)
+            bytes_read = len(chunk)
+            buf += chunk
+            if bytes_read < CHUNK_SIZE:
+                return buf
         except socket.error as e:
             err = e.args[0]
             if err == errno.EAGAIN:
