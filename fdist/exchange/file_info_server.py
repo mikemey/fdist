6de9479821bb78f84bb99e78788657c4b538ebba
mikemey <miguel.maestro@gmx.co.uk>
2017-01-27 19:06:58 +0000

    add cache hashing.

--- fdist/exchange/file_info_server.py
+++ fdist/exchange/file_info_server.py
@@ -13,6 +13,7 @@ class FileInfoServer(LogActor):
         super(FileInfoServer, self).__init__()
         self.local_dir = local_dir
         self.pip_size = pip_size
+        self.hash_cache = {}
 
     def on_receive(self, message):
         connection = message['connection']
@@ -28,6 +29,13 @@ class FileInfoServer(LogActor):
             connection.close()
 
     def hashes(self, file_id):
+        if file_id in self.hash_cache:
+            self.logger.debug('cache hit [%s]', file_id)
+        else:
+            self.hash_cache[file_id] = self.calculate_hash(file_id)
+        return self.hash_cache[file_id]
+
+    def calculate_hash(self, file_id):
         self.logger.debug("calculating hashes")
         full_path = self.local_dir + file_id
         pieces = int(ceil(os.path.getsize(full_path)) / float(self.pip_size))
@@ -37,8 +45,8 @@ class FileInfoServer(LogActor):
             pip = fin.read(self.pip_size)
             curr = 0
             while pip:
-                curr += 1
                 self.logger.debug("calculating %2s of %s", curr, pieces)
+                curr += 1
                 hashes.append(md5_hash(pip))
                 pip = fin.read(self.pip_size)
         return hashes
